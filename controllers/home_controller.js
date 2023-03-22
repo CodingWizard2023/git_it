5e050f60fcd8c1fac3802d3b72117f0c4e02c32d
NISCHINT SHRESHTA NIJAGUNA <nischintshreshtanijaguna@NISCHINTs-MacBook-Pro.local>
2023-03-23 00:11:11 +0530

    add a controller and a router for home

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,4 @@
 module.exports.home=function(req,res)
 {
-    return res.end('<h1>Exprees is up </h1>')
-}
-
-module.exports.RandomAction=function(req,res)
-{
-    return res.end('<h1>les go </h1>');
-}
+    return res.end('<h1>Express is up for codeial </h1>')
+}//this is a exporting a function outside the home_controller
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,6 @@
-const express=require('express');//fetch the existing instance 
+const express=require('express');
 const router=express.Router();
 const homeController=require('../controllers/home_controller');
-
-console.log('router loaded ');
-
-router.get('/',homeController.home);//accessing the controller
-router.get('/random',homeController.RandomAction);
-module.exports=router;  
-
+router.get('/',homeController.home);
+module.exports=router;
 
